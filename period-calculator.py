96889c7835c0a957e1334666c0a4a27055fc7df8
Dexter <118137332+Yeaaahhhhh@users.noreply.github.com>
2023-05-06 05:35:43 +0000

    add day of week column in csv and improve UI

--- period-calculator.py
+++ period-calculator.py
@@ -69,41 +69,61 @@ def generate_csv():
     csv_file = 'period_dates.csv'
     with open(csv_file, 'w', newline='') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(['Year', 'Month', 'Date'])
+        csvwriter.writerow(['Year', 'Month', 'Date', 'Day of the Week'])
         for period_date in future_period_dates:
-            csvwriter.writerow([period_date.year, period_date.month, period_date.day])
+            csvwriter.writerow([period_date.year, period_date.month, period_date.day, period_date.strftime('%A')])
 
     messagebox.showinfo("Success", f"Future period dates have been saved in {os.path.abspath(csv_file)}")
 
 root = tk.Tk()
 root.title("Girlfriend/Wife Period Calculator")
+root.geometry("700x400")
 
-birth_year_label = tk.Label(root, text="Enter your girlfriend's/wife's birth year (optional):")
-birth_year_entry = tk.Entry(root)
+# Create frames
+header_frame = tk.Frame(root)
+header_frame.pack(padx=10, pady=10)
+
+input_frame = tk.Frame(root)
+input_frame.pack(padx=10, pady=10)
+
+button_frame = tk.Frame(root)
+button_frame.pack(padx=10, pady=10)
+
+# Labels
+title_label = tk.Label(header_frame, text="Girlfriend/Wife Period Calculator", font=("Helvetica", 18))
+title_label.pack()
+
+birth_year_label = tk.Label(input_frame, text="Enter your girlfriend's/wife's birth year (optional):")
+period_start_label = tk.Label(input_frame, text="Enter the most current starting period date (YYYY, MM, DD):")
+period_interval_label = tk.Label(input_frame, text="Enter the interval between each period (normally between 21 to 35 days, average 28 days):")
+
+# Entry fields
+birth_year_entry = tk.Entry(input_frame)
 birth_year_entry.config(validate='key', validatecommand=(root.register(validate_birth_year), '%P'))
 
-period_start_label = tk.Label(root, text="Enter the most current starting period date (YYYY, MM, DD)")
-period_start_year_entry = tk.Entry(root, width=5)
-period_start_month_entry = tk.Entry(root, width=3)
-period_start_day_entry = tk.Entry(root, width=3)
+period_start_year_entry = tk.Entry(input_frame, width=7)
+period_start_month_entry = tk.Entry(input_frame, width=3)
+period_start_day_entry = tk.Entry(input_frame, width=3)
 
-period_interval_label = tk.Label(root, text="Enter the interval between each period (normally between 21 to 35 days, average 28 days):")
-period_interval_entry = tk.Entry(root)
+period_interval_entry = tk.Entry(input_frame)
 period_interval_entry.config(validate='key', validatecommand=(root.register(validate_period_interval), '%P'))
 
-generate_button = tk.Button(root, text="Generate CSV", command=generate_csv)
+# Button
+generate_button = tk.Button(button_frame, text="Generate CSV", command=generate_csv)
 
-birth_year_label.grid(row=0, column=0, sticky=tk.W)
-birth_year_entry.grid(row=1, column=0, sticky=tk.W)
+# Grid layout
+birth_year_label.grid(row=0, column=0, sticky="w", pady=(0, 5))
+birth_year_entry.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
-period_start_label.grid(row=2, column=0, sticky=tk.W)
-period_start_year_entry.grid(row=3, column=0, sticky=tk.W)
-period_start_month_entry.grid(row=3, column=1, sticky=tk.W)
-period_start_day_entry.grid(row=3, column=2, sticky=tk.W)
+period_start_label.grid(row=2, column=0, sticky="w", pady=(0, 5))
+period_start_year_entry.grid(row=3, column=0, padx=(10, 5), pady=5, sticky="we")
+period_start_month_entry.grid(row=3, column=1, padx=(5, 5), pady=5, sticky="w")
+period_start_day_entry.grid(row=3, column=2, padx=(5, 10), pady=5, sticky="w")
 
-period_interval_label.grid(row=4, column=0, sticky=tk.W)
-period_interval_entry.grid(row=5, column=0, sticky=tk.W)
+period_interval_label.grid(row=4, column=0, sticky="w", pady=(0, 5))
+period_interval_entry.grid(row=5, column=0, padx=10, pady=5, sticky="w")
 
-generate_button.grid(row=6, column=0, sticky=tk.W)
+# Button placement
+generate_button.pack(pady=10)
 
 root.mainloop()
